b97760e9313b1f339cc67b02defe208eb920cf41
zemanluky <zemanluky@gmail.com>
2024-11-24 15:19:25 +0100

    :sparkles: Add extra query filters for shopping list endpoint

--- src/schema/request/shopping-list.schema.ts
+++ src/schema/request/shopping-list.schema.ts
@@ -1,12 +1,19 @@
 import {paginatedQuerySchema} from "./paginated.schema.ts";
 import {z} from "zod";
 import {startOfDay} from "date-fns";
+import {coerceBoolean} from "../../utils/validations.utils.ts";
 
 export const shoppingListFilterQuerySchema = z.object({
+    // searches for shopping lists by name
     search: z.string().trim().optional(),
+    // searches for shopping lists of a given author
     author: z.string().trim().optional(),
-    includeOnly: z.enum(['own', 'shared', 'all']).default('all'),
-    completeBy: z.coerce.date().optional()
+    // searches for shopping lists that the user either owns, has access to or both
+    includeOnly: z.string().toLowerCase().trim().pipe(z.enum(['own', 'shared', 'all'])).default('all'),
+    // searches for shopping lists before a given completion date
+    completeBy: z.coerce.date().optional(),
+    // includes shopping lists that are completed
+    includeCompleted: z.preprocess(coerceBoolean, z.boolean()).default(false)
 });
 
 // filter schema for querying shopping lists
