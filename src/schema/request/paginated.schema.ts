71cfa3bf6ac08792c9216c96ba8b6738d4f7fa1e
zemanluky <zemanluky@gmail.com>
2024-11-24 15:38:58 +0100

    :bug: Fix string page and pageSize number params not being coerced to number correctly

--- src/schema/request/paginated.schema.ts
+++ src/schema/request/paginated.schema.ts
@@ -2,13 +2,8 @@ import {z} from "zod";
 
 /** Schema that should be used for any paginated endpoint. */
 export const paginatedQuerySchema = (defaultPageSize: number = 25) => z.object({
-    page: z.number()
-        .min(1)
-        .default(1),
-    pageSize: z.number()
-        .min(1)
-        .max(200)
-        .default(defaultPageSize)
+    page: z.coerce.number().min(1).default(1),
+    pageSize: z.coerce.number().min(1).max(200).default(defaultPageSize)
 });
 
 export type TPaginatedQuery = z.infer<ReturnType<typeof paginatedQuerySchema>>;
