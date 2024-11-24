2a2bbcfa71e470d91182a7812ba6439b2194b482
zemanluky <zemanluky@gmail.com>
2024-11-24 01:40:51 +0100

    :sparkles: Add shopping list member validation schemas

--- src/controller/shopping-list-item.controller.ts
+++ src/controller/shopping-list-item.controller.ts
@@ -2,6 +2,12 @@ import express, {type Response} from "express";
 import {authenticateRequest} from "../helper/request.guard.ts";
 import {bodyValidator, paramValidator, queryValidator} from "../helper/request.validator.ts";
 import type {IAppRequest} from "../../types";
+import {shoppingListDetailParamSchema, type TShoppingListDetailParams} from "../schema/request/shopping-list.schema.ts";
+import {
+    saveMembersBodySchema,
+    shoppingListMemberParamSchema, type TSaveMembersBody,
+    type TShoppingListMemberParams
+} from "../schema/request/shopping-list-member.schema.ts";
 
 // should be mounted already on a route containing the id parameter of the shopping list - we have to merge the parameters
 export const shoppingListItemController = express.Router({ mergeParams: true });
@@ -10,8 +16,8 @@ export const shoppingListItemController = express.Router({ mergeParams: true });
  * Adds one or more members to the shopping list.
  */
 shoppingListItemController.post(
-    '/', paramValidator(), bodyValidator(),
-    async (req: IAppRequest, res: Response) => {
+    '/', paramValidator(shoppingListDetailParamSchema), bodyValidator(saveMembersBodySchema),
+    async (req: IAppRequest<TShoppingListDetailParams,never,TSaveMembersBody>, res: Response) => {
 
     }
 );
@@ -20,8 +26,8 @@ shoppingListItemController.post(
  * Modifies member's permission in the shopping list.
  */
 shoppingListItemController.patch(
-    '/:memberId/permission', paramValidator(), bodyValidator(),
-    async (req: IAppRequest, res: Response) => {
+    '/:memberId/permission', paramValidator(shoppingListMemberParamSchema), bodyValidator(saveMembersBodySchema),
+    async (req: IAppRequest<TShoppingListMemberParams,never,TSaveMembersBody>, res: Response) => {
 
     }
 );
@@ -30,8 +36,8 @@ shoppingListItemController.patch(
  * Removes a member from the shopping list.
  */
 shoppingListItemController.delete(
-    '/:memberId', paramValidator(),
-    async (req: IAppRequest, res: Response) => {
+    '/:memberId', paramValidator(shoppingListMemberParamSchema),
+    async (req: IAppRequest<TShoppingListMemberParams>, res: Response) => {
 
     }
 );
---
+++ src/schema/request/shopping-list-member.schema.ts
@@ -0,0 +1,17 @@
+import {shoppingListDetailParamSchema} from "./shopping-list.schema.ts";
+import {z} from "zod";
+
+// schema for parameters when editing or deleting list member
+export const shoppingListMemberParamSchema = shoppingListDetailParamSchema.extend({
+    memberId: z.string().trim().min(1)
+});
+export type TShoppingListMemberParams = z.infer<typeof shoppingListMemberParamSchema>;
+
+// schema for adding or editing list members
+export const saveMembersBodySchema = z.object({
+    members: z.array(z.object({
+        user: z.string().trim().min(1),
+        permission: z.enum(['read', 'write'])
+    })).min(1),
+});
+export type TSaveMembersBody = z.infer<typeof saveMembersBodySchema>;
