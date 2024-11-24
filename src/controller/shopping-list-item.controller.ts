c110125c3914d2a20db31855c36c868717161ecd
zemanluky <zemanluky@gmail.com>
2024-11-24 15:36:23 +0100

    :bug: Fix endpoints switched between item and member controllers

--- src/controller/shopping-list-item.controller.ts
+++ src/controller/shopping-list-item.controller.ts
@@ -1,44 +1,54 @@
 import express, {type Response} from "express";
-import {authenticateRequest} from "../helper/request.guard.ts";
 import {bodyValidator, paramValidator, queryValidator} from "../helper/request.validator.ts";
 import type {IAppRequest} from "../../types";
-import {shoppingListDetailParamSchema, type TShoppingListDetailParams} from "../schema/request/shopping-list.schema.ts";
 import {
-    saveMembersBodySchema,
-    shoppingListMemberParamSchema, type TSaveMembersBody,
-    type TShoppingListMemberParams
-} from "../schema/request/shopping-list-member.schema.ts";
+    markItemBoughtBodySchema,
+    saveItemBodySchema,
+    shoppingListItemParamSchema, type TMarkItemBoughtBody, type TSaveItemBody,
+    type TShoppingListItemParams
+} from "../schema/request/shopping-list-item.schema.ts";
+import {shoppingListDetailParamSchema, type TShoppingListDetailParams} from "../schema/request/shopping-list.schema.ts";
 import {successResponse} from "../helper/response.helper.ts";
 
 // should be mounted already on a route containing the id parameter of the shopping list - we have to merge the parameters
 export const shoppingListItemController = express.Router({ mergeParams: true });
 
 /**
- * Adds one or more members to the shopping list.
+ * Creates a new item in a given shopping list.
  */
 shoppingListItemController.post(
-    '/', paramValidator(shoppingListDetailParamSchema), bodyValidator(saveMembersBodySchema),
-    async (req: IAppRequest<TShoppingListDetailParams,never,TSaveMembersBody>, res: Response) => {
+    '/', paramValidator(shoppingListDetailParamSchema), bodyValidator(saveItemBodySchema),
+    async (req: IAppRequest<TShoppingListDetailParams,never,TSaveItemBody>, res: Response) => {
         successResponse(res, { params: req.parsedParams, body: req.body });
     }
 );
 
 /**
- * Modifies member's permission in the shopping list.
+ * Modifies a given shopping list item.
  */
-shoppingListItemController.patch(
-    '/:memberId/permission', paramValidator(shoppingListMemberParamSchema), bodyValidator(saveMembersBodySchema),
-    async (req: IAppRequest<TShoppingListMemberParams,never,TSaveMembersBody>, res: Response) => {
+shoppingListItemController.put(
+    '/:itemId', paramValidator(shoppingListItemParamSchema), bodyValidator(saveItemBodySchema),
+    async (req: IAppRequest<TShoppingListItemParams,never,TSaveItemBody>, res: Response) => {
         successResponse(res, { params: req.parsedParams, body: req.body });
     }
 );
 
 /**
- * Removes a member from the shopping list.
+ * Deletes an item from the shopping list.
  */
 shoppingListItemController.delete(
-    '/:memberId', paramValidator(shoppingListMemberParamSchema),
-    async (req: IAppRequest<TShoppingListMemberParams>, res: Response) => {
+    '/:itemId', paramValidator(shoppingListItemParamSchema),
+    async (req: IAppRequest<TShoppingListItemParams>, res: Response) => {
         successResponse(res, { params: req.parsedParams });
+    }
+);
+
+/**
+ * Marks an item as bought.
+ */
+shoppingListItemController.patch(
+    '/:itemId/completed-status', paramValidator(shoppingListItemParamSchema), bodyValidator(markItemBoughtBodySchema),
+    async (req: IAppRequest<TShoppingListItemParams,never,TMarkItemBoughtBody>, res: Response) => {
+        successResponse(res, { params: req.parsedParams, body: req.body });
     }
 );
--- src/controller/shopping-list-member.controller.ts
+++ src/controller/shopping-list-member.controller.ts
@@ -2,54 +2,43 @@ import express, {type Response} from "express";
 import {authenticateRequest} from "../helper/request.guard.ts";
 import {bodyValidator, paramValidator, queryValidator} from "../helper/request.validator.ts";
 import type {IAppRequest} from "../../types";
-import {
-    markItemBoughtBodySchema,
-    saveItemBodySchema,
-    shoppingListItemParamSchema, type TMarkItemBoughtBody, type TSaveItemBody,
-    type TShoppingListItemParams
-} from "../schema/request/shopping-list-item.schema.ts";
 import {shoppingListDetailParamSchema, type TShoppingListDetailParams} from "../schema/request/shopping-list.schema.ts";
+import {
+    saveMembersBodySchema,
+    shoppingListMemberParamSchema, type TSaveMembersBody,
+    type TShoppingListMemberParams
+} from "../schema/request/shopping-list-member.schema.ts";
 import {successResponse} from "../helper/response.helper.ts";
 
 // should be mounted already on a route containing the id parameter of the shopping list - we have to merge the parameters
 export const shoppingListMemberController = express.Router({ mergeParams: true });
 
 /**
- * Creates a new item in a given shopping list.
+ * Adds one or more members to the shopping list.
  */
 shoppingListMemberController.post(
-    '/', paramValidator(shoppingListDetailParamSchema), bodyValidator(saveItemBodySchema),
-    async (req: IAppRequest<TShoppingListDetailParams,never,TSaveItemBody>, res: Response) => {
+    '/', paramValidator(shoppingListDetailParamSchema), bodyValidator(saveMembersBodySchema),
+    async (req: IAppRequest<TShoppingListDetailParams,never,TSaveMembersBody>, res: Response) => {
         successResponse(res, { params: req.parsedParams, body: req.body });
     }
 );
 
 /**
- * Modifies a given shopping list item.
- */
-shoppingListMemberController.put(
-    '/:itemId', paramValidator(shoppingListItemParamSchema), bodyValidator(saveItemBodySchema),
-    async (req: IAppRequest<TShoppingListItemParams,never,TSaveItemBody>, res: Response) => {
-        successResponse(res, { params: req.parsedParams, body: req.body });
-    }
-);
-
-/**
- * Deletes an item from the shopping list.
- */
-shoppingListMemberController.delete(
-    '/:itemId', paramValidator(shoppingListItemParamSchema),
-    async (req: IAppRequest<TShoppingListItemParams>, res: Response) => {
-        successResponse(res, { params: req.parsedParams });
-    }
-);
-
-/**
- * Marks an item as bought.
+ * Modifies member's permission in the shopping list.
  */
 shoppingListMemberController.patch(
-    '/:itemId/completed-status', paramValidator(shoppingListItemParamSchema), bodyValidator(markItemBoughtBodySchema),
-    async (req: IAppRequest<TShoppingListItemParams,never,TMarkItemBoughtBody>, res: Response) => {
+    '/:memberId/permission', paramValidator(shoppingListMemberParamSchema), bodyValidator(saveMembersBodySchema),
+    async (req: IAppRequest<TShoppingListMemberParams,never,TSaveMembersBody>, res: Response) => {
         successResponse(res, { params: req.parsedParams, body: req.body });
+    }
+);
+
+/**
+ * Removes a member from the shopping list.
+ */
+shoppingListMemberController.delete(
+    '/:memberId', paramValidator(shoppingListMemberParamSchema),
+    async (req: IAppRequest<TShoppingListMemberParams>, res: Response) => {
+        successResponse(res, { params: req.parsedParams });
     }
 );
